61eb14c25a2d45ed62e4f9a5fddbfe7490005f90
Guigz2 <guillaume.egu@gmail.com>
2024-02-19 15:31:01 +0100

    Double Slider rotation X rotation Z

--- tp2/tp2/lib/main.dart
+++ tp2/tp2/lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
+import 'dart:math' as math;
+
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MonApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MonApp extends StatefulWidget{
+  @override
+  MainApp createState() => MainApp();
+}
+
+class MainApp extends State<MonApp> {
+  double _angle_x = 0;
+  double _angle_z = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +28,36 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('asterix_affiche.jpg')
-            ]
+                Expanded(
+                  child : Transform(
+                    transform: Matrix4.identity()
+                      ..rotateX(_angle_x)
+                      ..rotateZ(_angle_z),
+                    alignment: Alignment.center,
+                    child: Image.asset('asterix_affiche.jpg'),
+                  ),
+                ),
+                Slider(
+                  min:0,
+                  max: math.pi,
+                  value: _angle_x,
+                  onChanged: (double value){
+                    setState((){
+                      _angle_x = value;
+                    });
+                  },
+                ),
+                Slider(
+                  min: 0,
+                  max: math.pi, 
+                  value: _angle_z,
+                  onChanged: (double value) {
+                    setState(() {
+                      _angle_z = value;
+                    });
+                  },
+                ),
+              ]
             )
         ),
       ),

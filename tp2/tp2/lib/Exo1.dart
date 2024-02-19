3af9a407717a7672d1e34ceff20354d7d48c9138
Guigz2 <guillaume.egu@gmail.com>
2024-02-19 17:44:49 +0100

    Modification de chaque page pour avoir un bouton retour automatique (Suppression des return MaterialApp en return Scaffold pour ne pas écraser une liste de navigation sur une autre)

--- tp2/tp2/lib/Exo1.dart
+++ tp2/tp2/lib/Exo1.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
 
+void main() {
+  runApp(MaterialApp(home: Exo1Page(),
+  ));
+}
+
 
 class Exo1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Voici une image'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded( 
-                child: Image.asset('asterix_affiche.jpg'),
+    return Scaffold(
+              appBar: AppBar(          
+                title: Text('Voici une image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded( 
+              child: Image.asset('asterix_affiche.jpg'),              
               ),
-            ],
-          ),
+          ],
         ),
       ),
     );
--- tp2/tp2/lib/Exo2.dart
+++ tp2/tp2/lib/Exo2.dart
@@ -5,7 +5,7 @@ import 'dart:math' as math;
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MonApp());
+  runApp(MaterialApp(home:MonApp()));
 }
 
 class MonApp extends StatefulWidget{
@@ -21,94 +21,92 @@ class Exo2Page extends State<MonApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar : AppBar(
-          title: Text("Voici une image"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                Expanded(
-                  child : Container(
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(color: Colors.white),
-                      child : Transform(
-                        transform: Matrix4.identity()
-                          ..rotateX(_angle_x)
-                          ..rotateZ(_angle_z)
-                          ..scale(_isMirrored ? -_scale:_scale, _scale),
-                        alignment: Alignment.center,
-                        child: Image.asset('asterix_affiche.jpg'),
-                     ),
+    return Scaffold(
+      appBar : AppBar(
+        title: Text("Voici une image"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+              Expanded(
+                child : Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(color: Colors.white),
+                    child : Transform(
+                      transform: Matrix4.identity()
+                        ..rotateX(_angle_x)
+                        ..rotateZ(_angle_z)
+                        ..scale(_isMirrored ? -_scale:_scale, _scale),
+                      alignment: Alignment.center,
+                      child: Image.asset('asterix_affiche.jpg'),
+                    ),
+                ),
+              ),
+              Row( 
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("RotationX :"),
+                  Expanded( 
+                    child : Slider(
+                      min:0,
+                      max: math.pi,
+                      value: _angle_x,
+                      onChanged: (double value){
+                        setState((){
+                          _angle_x = value;
+                        });
+                      },
+                    ),
                   ),
-                ),
-                Row( 
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("RotationX :"),
-                    Expanded( 
-                      child : Slider(
-                        min:0,
-                        max: math.pi,
-                        value: _angle_x,
-                        onChanged: (double value){
-                          setState((){
-                            _angle_x = value;
-                          });
-                        },
-                      ),
+                ]
+              ),
+              Row( 
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("RotationZ :"),
+                  Expanded( 
+                    child : Slider(
+                      min:0,
+                      max: math.pi,
+                      value: _angle_z,
+                      onChanged: (double value){
+                        setState((){
+                          _angle_z = value;
+                        });
+                      },
                     ),
-                  ]
-                ),
-                Row( 
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("RotationZ :"),
-                    Expanded( 
-                      child : Slider(
-                        min:0,
-                        max: math.pi,
-                        value: _angle_z,
-                        onChanged: (double value){
-                          setState((){
-                            _angle_z = value;
-                          });
-                        },
-                      ),
+                  ),
+                ]
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isMirrored = !_isMirrored; 
+                  });
+                },
+                child: Text(_isMirrored ? 'Normal' : 'Miroir'), 
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Scale: '),
+                  Expanded(
+                    child: Slider(
+                      min: 0, 
+                      max: 2.0, 
+                      value: _scale,
+                      onChanged: (double value) {
+                        setState(() {
+                          _scale = value;
+                        });
+                      },
                     ),
-                  ]
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _isMirrored = !_isMirrored; 
-                    });
-                  },
-                  child: Text(_isMirrored ? 'Normal' : 'Miroir'), 
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Scale: '),
-                    Expanded(
-                      child: Slider(
-                        min: 0, 
-                        max: 2.0, 
-                        value: _scale,
-                        onChanged: (double value) {
-                          setState(() {
-                            _scale = value;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ]
-            )
-        ),
+                  ),
+                ],
+              ),
+            ]
+          )
       ),
     );
   }
